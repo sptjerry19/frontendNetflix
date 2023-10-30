ce9ebcfb12dd79f5f1a40f470d25cdecc3bc359a
sptjerry19 <duylinhvnu@gmail.com>
2023-10-31 00:40:56 +0700

    update url image

--- src/components/SliderItem.vue
+++ src/components/SliderItem.vue
@@ -4,7 +4,9 @@
       <div class="max-h-screen h-full">
         <img
           class="w-full max-h-3/4 min-h-1/2 object-cover"
-          :src="'http://localhost:8000/storage/' + film.image"
+          :src="
+            'https://jerrynetflixapp.000webhostapp.com/storage/' + film.image
+          "
           alt=""
         />
       </div>
